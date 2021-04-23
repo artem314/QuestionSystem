20c803309dcc93f68f7eea7f4d602db911f6e00e
artem314 <artem_pochta_314@mail.ru>
2021-04-23 23:37:47 +0300

    вынесено в цикл

--- QuestionSystem/Areas/Question/Controllers/Question/QuestionController.cs
+++ QuestionSystem/Areas/Question/Controllers/Question/QuestionController.cs
@@ -21,12 +21,12 @@ namespace QuestionSystem.Areas.Question.Controllers.Question
         public IActionResult Generate(string text)
         {
             Dictionary<string, string> CaseToquestion = new Dictionary<string, string>();
-            CaseToquestion.Add("им", "Что?"); //на самом деле его быть не должно, но программа не отличит винительный от именительного, а вопросы одинаковые, так что все ок
-            CaseToquestion.Add("рд", "Чего?");
-            CaseToquestion.Add("дт", "Чему?");
-            CaseToquestion.Add("вн", "Что?");
-            CaseToquestion.Add("тв", "Чем?");
-            CaseToquestion.Add("пр", "О чем?");
+            CaseToquestion.Add("им", "Что"); //на самом деле его быть не должно, но программа не отличит винительный от именительного, а вопросы одинаковые, так что все ок
+            CaseToquestion.Add("рд", "Чего");
+            CaseToquestion.Add("дт", "Чему");
+            CaseToquestion.Add("вн", "Что");
+            CaseToquestion.Add("тв", "Чем");
+            CaseToquestion.Add("пр", "О чем");
 
          //   /question/question
 
@@ -94,11 +94,59 @@ namespace QuestionSystem.Areas.Question.Controllers.Question
 
             */
 
-            string test = "";
-
             foreach (KeyValuePair<int, GenQuestion> sug in suggestions)
             {
-                test += sug.Value.Suggestion + " ";
+
+                short subject = 0; // подлежащее
+                short predicate = 0;// сказуемое
+                /// 0 -  вопросительное слово
+                /// 1 - глагол
+                /// 2 - существительное
+                /// 
+                List<string> result = new List<string>(3);
+                result.Add("");
+                result.Add("");
+                result.Add("");
+
+                //разбиение предложения на слова
+                string[] words = sug.Value.Suggestion.Split(delimiterChars,StringSplitOptions.RemoveEmptyEntries);
+
+                var morphResult = morph.Parse(words).ToArray();
+                string wordCase = string.Empty;
+
+
+                foreach (DeepMorphy.Model.MorphInfo morphInfo in morphResult)
+                {
+
+                    if (morphInfo.BestTag.Has("сущ"))
+                    {
+
+                        if (subject == 0)
+                        {
+                            wordCase = morphInfo.BestTag["падеж"];
+                            subject++;
+                            result[2] = morphInfo.Text;
+                        }
+
+                        //следующее после глагола существительное
+                        if (subject == 1 && predicate != 0 && !String.IsNullOrEmpty(wordCase))
+                        {
+                            result[0] = CaseToquestion[wordCase];
+                        }
+
+                    }
+
+                    if (morphInfo.BestTag.Has("гл") && predicate < 1)
+                    {
+                        result[1] = morphInfo.Text;
+                        predicate++;
+                    }
+                }
+
+               
+                //добавить проверку, если вопрос не составлен (меньше 3 слов) удалить вырождение
+                sug.Value.QuestionText = string.Join(" ",result) + '?';
+
             }
 
 
